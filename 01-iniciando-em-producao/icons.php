0849851c793994fd71c5160ba1004e526fd6fa67
Fernando <death_dan@hotmail.com>
2023-04-10 19:18:19 -0300

    Links e Botões

--- 01-iniciando-em-producao/icons.php
+++ 01-iniciando-em-producao/icons.php
@@ -35,17 +35,60 @@
             margin: 20px 0 10px 0;
         }
 
+        h1 {
+            font-size: 2em;
+            font-weight: 700;
+            margin: 10px 0;
+        }
+
+        p {
+            margin: 10px 0;
+        }
+
     </style>
 </head>
 <body>
     <div class="box">
         <div class="box_container">
-            <h1>ICONES</h1>
+            <h1>ICONES:</h1>
             <p class="icon-facebook">Facebook</p>
             <p class="icon-instagram">Instagram</p>
             <p class="icon-youtube">YouTube</p>
             <p class="icon-search">Search</p>
+
+            <h1>LINKS:</h1>
+            <p><a href="#" title="">Facebook</a></p>
+            <p><a href="#" title="" class="icon-facebook">Facebook</a></p>
+
+            <h1>BOTÔES:</h1>
+            <a href="" title="" class="btn radius">Link Button</a>
+            <button class="btn radius transition">Button</button>
+            <span class="btn radius transition icon-search">Span Button</span>
+
+            <p>Tamanhos</p>
+            <a href="" title="" class="btn radius btn-small">Link Button</a>
+            <a href="" title="" class="btn radius">Link Button</a>
+            <a href="" title="" class="btn radius btn-big">Link Button</a>
+
+            <p>Cores</p>
+            <p>
+                <span class="btn btn-green radius transition icon-search">Span Button</span>
+                <span class="btn btn-green-out radius transition icon-search">Span Button</span>
+            </p>
+            <p>
+                <span class="btn btn-blue-out radius transition icon-search">Span Button</span> 
+                <span class="btn btn-blue radius transition icon-search">Span Button</span>      
+            </p>
+            <p>
+                <span class="btn btn-yellow radius transition icon-search">Span Button</span>
+                <span class="btn btn-yellow-out radius transition icon-search">Span Button</span>       
+            </p>
+            <p>
+                <span class="btn btn-red-out radius transition icon-search">Span Button</span> 
+                <span class="btn btn-red radius transition icon-search">Span Button</span>     
+            </p>
         </div>
     </div>
+    
 </body>
 </html>
