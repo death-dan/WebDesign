ab9a59d0f54c021cbf946e9ad2dd36a6695216f3
Fernando <death_dan@hotmail.com>
2023-04-11 13:13:48 -0300

    Header, menu e visual

--- 02-iniciando-projeto/index.php
+++ 02-iniciando-projeto/index.php
@@ -15,10 +15,55 @@
 
 </head>
 <body>
-    <header class="main_header"></header>
+    <header class="main_header">
+        <div class="container">
+            <a href="#" title="WdpShoes | Home">
+                <img src="Themes/WdpShoes/Images/wdpshoes_logo_white.png" alt="" title="">
+            </a>
+            <div class="main_header_search">
+                <form action="" method="post" class="radius">
+                    <input type="text" name="" placeholder="Pesquisar na WdpShoes">
+                    <button class="icon-search icon-notext transition"></button>
+                </form>
+            </div>
+            <div class="main_header_user">
+                <a href="" title="" class="icon-user radius transition">Minha Conta</a>
+                <ul class="radius">
+                    <li><a href="#" title="">Meus pedidos</a></li>
+                    <li><a href="#" title="">Meus dados</a></li>
+                    <li><a href="#" title="">Meus endesreços</a></li>
+                    <li><a href="#" title="">Sair</a></li>      
+                </ul>
+            </div> 
+            <nav class="main_header_nav">
+                <ul>
+                    <?php 
+                        for($i = 1; $i < 5; $i++) {
+                    ?>
+                            <li>
+                                Departamento <?= $i;?>
+                                <ul>
+                                <?php 
+                                    for($ii = 1; $ii < 5; $ii++) {
+                                ?>
+                                        <li><a href="#" title="">Categoria <?= "{$i}/{$ii}";?></a>
+                                <?php        
+                                    }
+                                ?>
+                                </ul>
+                            </li>
+                    <?php        
+                        }
+                    ?>
+                </ul>
+            </nav>    
+        </div>
+    </header>
+
     <main class="main_content">
         <h1>Vamos Programar!!!!</h1>
     </main>
     <footer class="main_footer"></footer>
+    
 </body>
 </html>
