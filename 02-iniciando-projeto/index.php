aac7f38ab6b7aef67cfc434727769140dda06e70
Fernando <death_dan@hotmail.com>
2023-04-10 22:16:55 -0300

    Definindo Escopo Geral

--- 02-iniciando-projeto/index.php
+++ 02-iniciando-projeto/index.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>WdpShoes | Home</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="Assets/Styles/Icons.css">
+    <link href="https://file.myfontastic.com/d3XTWRRNXU2GYaFhhAUY4W/icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="Assets/Styles/Boot.css">
+    <link rel="stylesheet" href="Themes/WdpShoes/Style.css">
+    <link rel="shortcut icon" href="Themes/WdpShoes/Images/favicon.png">
+
 </head>
 <body>
-    
+    <header class="main_header"></header>
+    <main class="main_content">
+        <h1>Vamos Programar!!!!</h1>
+    </main>
+    <footer class="main_footer"></footer>
 </body>
 </html>
