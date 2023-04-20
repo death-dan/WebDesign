66583f130863f1762ff7b22584a5e68039c99c95
Fernando <death_dan@hotmail.com>
2023-04-20 11:27:16 -0300

    Recursos nativos

--- 02-iniciando-projeto/index.php
+++ 02-iniciando-projeto/index.php
@@ -67,11 +67,11 @@
 
     <main class="main_content">
         <div class="main_slide j_slide">
-            <a href="#" title="" class="j_slide_item"><img src="Uploads/featured-02.jpg" alt="" title=""></a>
+            <a href="#" title="" class="j_slide_item d_none"><img src="Uploads/featured-02.jpg" alt="" title=""></a>
             <?php 
                 for ($i = 1; $i < 4; $i++) {
             ?>
-                    <a href="#" title="" class="j_slide_item d_none"><img src=<?= "Uploads/featured-0{$i}.jpg"; ?> alt="" title=""></a>
+                    <a href="#" title="" class="j_slide_item"><img src=<?= "Uploads/featured-0{$i}.jpg"; ?> alt="" title=""></a>
             <?php
                 }
             ?>
@@ -142,6 +142,93 @@
                             }
                         ?>
                     </div>
+            </div>
+        </section>
+
+        <section class="pdt_gallery pdt_select">
+            <div class="container">
+                <header class="pdt_select_header">
+                    <h1>
+                        <span class="j_tabs_nav transition radius">Eles</span>
+                        <span class="j_tabs_nav transition radius">Elas</span>
+                    </h1>
+                </header>
+
+                <div class="j_tabs">
+                    <div class="j_tabs_item">
+                        <div class="flex">
+                            <div class="pdt_gallery_list pdt_gallery_featured">
+                                <article class="pdt_gallery_item flex-2">
+                                    <a href="#" title=""><img src="Uploads/products/15.jpg" alt="" title=""></a>
+                                    <header class="pdt_gallery_item_desc">
+                                        <h2 class="pdt_gallery_item_desc_title"><a href="#" class="transition" title="Calça Moletom Confort Mama Latina">Calça Moletom Confort Mama Latina</a></h2>
+                                        <div class="pdt_gallery_item_desc_price">
+                                            <p><b>R$ 59,90</b> <span>R$ 99,90</span></p>
+                                        </div>
+                                        <p>2x R$ 29,95</p>
+                                    </header>
+                                </article>
+                            </div>
+
+                            <div class="pdt_gallery_list flex">
+                                <?php 
+                                    $pdt = 0;
+                                    for($pdt = 11; $pdt < 15; $pdt++) {
+                                ?>
+                                        <article class="pdt_gallery_item flex-2">
+                                            <a href="#" title=""><img src="Uploads/products/<?= str_pad($pdt, 2, 0, STR_PAD_LEFT); ?>.jpg" alt="" title=""></a>
+                                            <header class="pdt_gallery_item_desc">
+                                                <h2 class="pdt_gallery_item_desc_title"><a href="#" class="transition" title="Calça Moletom Confort Mama Latina">Calça Moletom Confort Mama Latina</a></h2>
+                                                <div class="pdt_gallery_item_desc_price">
+                                                    <p><b>R$ 59,90</b> <span>R$ 99,90</span></p>
+                                                </div>
+                                                <p>2x R$ 29,95</p>
+                                            </header>
+                                        </article>
+                                <?php 
+                                    }
+                                ?>
+                            </div>
+                        </div> 
+                    </div>
+
+                    <div class="j_tabs_item">
+                        <div class="flex">
+                            <div class="pdt_gallery_list pdt_gallery_featured">
+                                <article class="pdt_gallery_item flex-2">
+                                    <a href="#" title=""><img src="Uploads/products/09.jpg" alt="" title=""></a>
+                                    <header class="pdt_gallery_item_desc">
+                                        <h2 class="pdt_gallery_item_desc_title"><a href="#" class="transition" title="Calça Moletom Confort Mama Latina">Calça Moletom Confort Mama Latina</a></h2>
+                                        <div class="pdt_gallery_item_desc_price">
+                                            <p><b>R$ 59,90</b> <span>R$ 99,90</span></p>
+                                        </div>
+                                        <p>2x R$ 29,95</p>
+                                    </header>
+                                </article>
+                            </div>
+
+                            <div class="pdt_gallery_list flex">
+                                <?php 
+                                    $pdt = 0;
+                                    for($pdt = 5; $pdt < 9; $pdt++) {
+                                ?>
+                                        <article class="pdt_gallery_item flex-2">
+                                            <a href="#" title=""><img src="Uploads/products/<?= str_pad($pdt, 2, 0, STR_PAD_LEFT); ?>.jpg" alt="" title=""></a>
+                                            <header class="pdt_gallery_item_desc">
+                                                <h2 class="pdt_gallery_item_desc_title"><a href="#" class="transition" title="Calça Moletom Confort Mama Latina">Calça Moletom Confort Mama Latina</a></h2>
+                                                <div class="pdt_gallery_item_desc_price">
+                                                    <p><b>R$ 59,90</b> <span>R$ 99,90</span></p>
+                                                </div>
+                                                <p>2x R$ 29,95</p>
+                                            </header>
+                                        </article>
+                                <?php 
+                                    }
+                                ?>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
             </div>
         </section>
     </main>
