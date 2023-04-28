d48622c30e377cadbb10401699be67e34935cd9a
Fernando <death_dan@hotmail.com>
2023-04-28 17:16:53 -0300

    Criando 05 Página de Categoria e Pesquisa

---
+++ 05-pagina-categoria-e-pesquisa/Themes/WdpShoes/header.php
@@ -0,0 +1,51 @@
+<header class="main_header">
+    <div class="container">
+        <div class="main_header_content">
+            <div class="logo">
+                <a href="<?= $configBase ;?>" title="WdpShoes | Home">
+                    <img src="<?= $configThemeLink ;?>/Images/wdpshoes_logo_white.png" alt="" title="">
+                </a>
+            </div> 
+            <div class="main_header_search">
+                <form action="" method="post" class="radius">
+                    <input type="text" name="" placeholder="Pesquisar na WdpShoes">
+                    <button class="icon-search icon-notext transition"></button>
+                </form>
+            </div>
+            <div class="main_header_menu">
+                <a href="#" title="" class="icon-cart icon-notext transition"><span>3</span></a>
+                <div class="main_header_menu_user">
+                    <a href="<?= $configBase ;?>/conta" title="" class="icon-user radius transition">Minha Conta</a>
+                    <div class="radius nav">
+                        <a href="<?= $configBase ;?>/conta" title="">Meus pedidos</a>
+                        <a href="<?= $configBase ;?>/conta/cadastro" title="">Meus dados</a>
+                        <a href="<?= $configBase ;?>/conta/enderecos" title="">Meus endesreços</a>
+                        <a href="<?= $configBase ;?>/conta/sair" title="">Sair</a>     
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <nav class="main_header_nav">
+            <ul class="menu">
+                <?php 
+                    for($i = 1; $i < 5; $i++) {
+                ?>
+                        <li>
+                            Departamento <?= $i;?>
+                            <ul class="sub_menu">
+                            <?php 
+                                for($ii = 1; $ii < 5; $ii++) {
+                            ?>
+                                    <li><a href="<?= $configBase ;?>/categoria" title="">Categoria <?= "{$i}/{$ii}";?></a>
+                            <?php        
+                                }
+                            ?>
+                            </ul>
+                        </li>
+                <?php        
+                    }
+                ?>
+            </ul>
+        </nav>    
+    </div>
+</header>
---
+++ 05-pagina-categoria-e-pesquisa/index.php
@@ -0,0 +1,57 @@
+<?php
+    define("BASE", "https://localhost/GitEssentials/WebDesign/05-pagina-categoria-e-pesquisa");
+    define("THEME", "WdpShoes");
+    define("THEME_PATH", __DIR__ . "/Themes/" . THEME);
+    define("THEME_LINK", BASE . "/Themes/" . THEME);
+
+    $configBase = BASE;
+    $configUrl = explode("/", strip_tags(filter_input(INPUT_GET, "url", FILTER_DEFAULT)));
+    $configUrl[0] = (!empty($configUrl[0]) ? $configUrl[0] : "index");
+    $configThemePath = THEME_PATH;
+    $configThemeLink = THEME_LINK;
+    $configSiteName = "WdpShoes";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WdpShoes | Home</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= $configBase ;?>/Assets/Styles/Icons.css">
+    <link rel="stylesheet" href="<?= $configBase ;?>/Assets/Styles/Boot.css">
+    <link rel="stylesheet" href="<?= $configThemeLink ;?>/Style.css">
+    <link rel="shortcut icon" href="<?= $configThemeLink ;?>/Images/favicon.png">
+
+    <script src="<?= $configBase ;?>/Assets/Js/jquery.js"></script>
+    <script src="<?= $configBase ;?>/Assets/Js/script.js"></script>
+</head>
+<body>
+    <?php
+        
+        //HEADER
+        require "{$configThemePath}/header.php";
+
+        //QUERY STRING
+        if (file_exists("{$configThemePath}/{$configUrl[0]}.php") && !is_dir("{$configThemePath}/{$configUrl[0]}.php")) {
+            //theme root
+            require "{$configThemePath}/{$configUrl[0]}.php";
+        } elseif (!empty($configUrl[1]) && file_exists("{$configThemePath}/{$configUrl[0]}/{$configUrl[1]}.php") && !is_dir("{$configThemePath}/{$configUrl[0]}/{$configUrl[1]}.php")) {
+            //theme folder
+            require "{$configThemePath}/{$configUrl[0]}/{$configUrl[1]}.php";
+        } else {
+            //error 404
+            if (file_exists("{$configThemePath}/404.php") &&  !is_dir("{$configThemePath}/404.php")) {
+                require "{$configThemePath}/404.php";
+            } else {
+                echo "<div class='container'><div class='trigger trigger-error icon-error radius'>Desculpe mas a página que você tento acessar não existe ou foi removida!</div></div>";
+            }
+        }
+
+        //FOOTER
+        require "{$configThemePath}/footer.php";
+    ?>
+</body>
+</html>
